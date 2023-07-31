2c3faff1c285a457a2ab0edf7985c4fb5c9bff60
boldPro <probold777@gmail.com>
2023-07-30 22:05:15 -0400

    moked first home section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,30 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <main id="main" className='PageLayoutWrapper'>
+        <div className="SectionCenter SectionFlex">
+            <div className="FirstWrapper">
+
+                <div className="ImageContainer">
+                    <picture className="NextGenImage">
+                        <img src="https://images.ctfassets.net/c5bd0wqjc7v0/5oEZBTPlhzKLA5OrNpRmsl/763c828277bea2afdb4c981441dd700f/HeroImg.webp?fl=progressive&q=100&w=1180" alt="" />
+                    </picture>
+                </div>
+
+                <div className="TextWrapper SectionFlex SectionCenter">
+                    <div className='TitleWrapper SectionFlex SectionRow'>
+                        <div className="SectionFlex SectionMaxWidthHundred">
+                            <h1>The future of money is here</h1>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+    </main>
   )
 }
