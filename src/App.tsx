70360b1a998bfbe23437799b6200e290997fe6a9
boldPro <probold777@gmail.com>
2023-08-18 21:18:33 -0400

    switching between mobile and desctop resize listener

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,29 @@ import SignUpPage from './pages/SignUpPage';
 
 
 function App() {
-
+  let [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  
   const homePageCheckr = useRef<HTMLDivElement>(null)
   const headerCheckr = useRef<HTMLDivElement>(null)
 
   let[slidingHeader, setSlidingHeader] = useState(false)
 
+  const handleResize=()=>{
+    setWindowWidth(window.innerWidth)
+  }
+
   useEffect(()=>{
-      const headerObserver = new IntersectionObserver((entries)=>{
-        const header = entries[0]
-        if (!header.isIntersecting)setSlidingHeader(true)
-      })
-      const homePageObserver = new IntersectionObserver((entries)=>{
-           const homepage = entries[0]
-             if(homepage.isIntersecting)setSlidingHeader(false)
-      })
-      if (headerCheckr.current)headerObserver.observe(headerCheckr.current)
-      if (homePageCheckr.current)homePageObserver.observe(homePageCheckr.current)
+    window.addEventListener("resize", handleResize)
+    const headerObserver = new IntersectionObserver((entries)=>{
+      const header = entries[0]
+      if (!header.isIntersecting)setSlidingHeader(true)
+    })
+    const homePageObserver = new IntersectionObserver((entries)=>{
+          const homepage = entries[0]
+            if(homepage.isIntersecting)setSlidingHeader(false)
+    })
+    if (headerCheckr.current)headerObserver.observe(headerCheckr.current)
+    if (homePageCheckr.current)homePageObserver.observe(homePageCheckr.current)
   },[])
 
 
@@ -35,7 +41,7 @@ function App() {
       <Header slidingHeader={slidingHeader} headerCheckr={headerCheckr} />
       <Routes>
         {/* send email on first Wrapper to sign up page  */}
-        <Route path="/" element = {<HomePage slidingHeader={slidingHeader} homePageCheckr={homePageCheckr}/>}/>
+        <Route path="/" element = {<HomePage windowWidth={windowWidth} slidingHeader={slidingHeader} homePageCheckr={homePageCheckr}/>}/>
         <Route path="/signup" element={<SignUpPage/>}/> 
       </Routes>
      
