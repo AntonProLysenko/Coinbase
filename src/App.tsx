522a81207be7674ea72dd4206caa164587a58309
boldPro <probold777@gmail.com>
2023-08-27 16:08:25 -0400

    Connected Coin API

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Header from "./components/Header"
 import HomePage from "./pages/HomePage"
 import SignUpPage from './pages/SignUpPage';
 
+const KEY = process.env.REACT_APP_KEY;
+const BASIC_URL = process.env.REACT_APP_BASIC_URL
+
 
 function App() {
   let [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -22,6 +25,30 @@ function App() {
     setWindowWidth(window.innerWidth)
   }
 
+
+ 
+fetch(`${BASIC_URL}/exchangerate/BTC/USD`, {
+  headers: {
+    "X-CoinAPI-Key": `${KEY}` // Replace with your API key
+    }
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
+
+
+
+ const getCoin = async()=>{
+        try{
+            const response = await fetch(`${BASIC_URL}/exchangerate/BTC/USD?apikey=${KEY}`)
+            const data = await response.json();
+            
+            console.log("Personal",data);
+
+        }catch(e){
+            console.error(e)
+        }
+     }
   useEffect(()=>{
     window.addEventListener("resize", handleResize)
     const headerObserver = new IntersectionObserver((entries)=>{
@@ -40,6 +67,7 @@ function App() {
   return (
     <div className="App">
       <Header slidingHeader={slidingHeader} headerCheckr={headerCheckr} isMobileActive={isMobileActive} setMobileActive={setMobileActive} />
+      <button onClick={getCoin}>Fetch</button>
       <Routes>
         {/* send email on first Wrapper to sign up page  */}
         <Route path="/" element = {<HomePage windowWidth={windowWidth} slidingHeader={slidingHeader} isMobileActive={isMobileActive} setMobileActive={setMobileActive}  homePageCheckr={homePageCheckr}/>}/>
